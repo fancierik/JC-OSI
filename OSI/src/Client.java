ee0a3e3aafb4d8e2e96569478866a9faddbf2aa5
Ann Ovcharuk <anblackdiamond@gmail.com>
2022-10-13 12:24:48 +0300

    add

--- OSI/src/Client.java
+++ OSI/src/Client.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Client {
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.BufferOverflowException;
+
+public class Client {
+
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 8080;
+
+        try (Socket socket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));) {
+
+            out.println("Netology");
+
+            System.out.println(in.readLine());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
